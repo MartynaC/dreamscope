b6644b9578aa1d3e119a280ca21e9e0980fb70d4
martyna chojnacka <hello@martynachojnacka.com>
2026-03-30 17:09:09 +0200

    add dream matcher with sentence transformers

---
+++ dataset_matching/dream_matcher.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("all-mpnet-base-v2")
+
+ #Run this code once when dataset is updated or when model has changed
+ #to create the embeddings for the dream symbols and interpretations, 
+ #then comment it out to avoid re-running it every time.
+
+#df = pd.read_csv("raw_data/dreams_interpretations.csv")
+#print(df.head())
+#print(df.columns)
+#
+#texts = (df["Dream Symbol"] + " " + df["Interpretation"]).tolist()
+#print(texts[0])
+#
+#embeddings = model.encode(texts, show_progress_bar=True)
+#print(embeddings.shape)
+#
+#np.save("raw_data/symbol_embeddings.npy", embeddings)
+#print("saved")
+
+def match_dream(dream_text, top_k=5): # 5 best matches
+    df = pd.read_csv("raw_data/dreams_interpretations.csv")
+    embeddings = np.load("raw_data/symbol_embeddings.npy")
+    
+    query_embedding = model.encode([dream_text])
+    similarities = np.dot(embeddings, query_embedding.T).flatten()
+    top_indices = np.argsort(similarities)[-top_k:][::-1]
+    
+    results = []
+    for idx in top_indices:
+        if similarities[idx] > 0.1:  # Only include results with a similarity score above a certain threshold
+            results.append({
+                "Dream Symbol": df.iloc[idx]["Dream Symbol"],
+                "Interpretation": df.iloc[idx]["Interpretation"],
+                "score": round(float(similarities[idx]), 4)  
+                })
+    return results
+
+
+if __name__ == "__main__":
+    dream =  "i dreamed about a ladybug"
+    results = match_dream(dream)
+    
+    for r in results:
+        print(f"\n{r['Dream Symbol']} (score: {r['score']})")
+        print(r['Interpretation'])
