ddba19ec30045c077f151907ec9934d9c05f12de
Benjamin Devertu <bajou999@yahoo.fr>
2026-04-03 16:46:48 +0200

    Containerized the API but deployment issue

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.10-slim
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY dreamscope_backend dreamscope_backend
+
+# For local development, you can use the following command to run the API server:
+#CMD uvicorn dreamscope_backend.api_file:app --host 0.0.0.0
+
+# For deployment on Cloud, use the following command to run the API server:
+CMD uvicorn dreamscope_backend.api_file:app --host 0.0.0.0 --port $PORT
--- Notebooks/dataset_matching/dream_matcher.py
+++ Notebooks/dataset_matching/dream_matcher.py
@@ -9,7 +9,7 @@ model = SentenceTransformer("all-mpnet-base-v2")
 lemmatizer = WordNetLemmatizer()
 
  #Run this code once when dataset is updated or when model has changed
- #to create the embeddings for the dream symbols and interpretations, 
+ #to create the embeddings for the dream symbols and interpretations,
  #then comment it out to avoid re-running it every time.
 
 #df = pd.read_csv("raw_data/dreams_interpretations.csv")
@@ -31,20 +31,20 @@ def lemmatize(text):
 
 def match_dream(dream_text, top_k=5): # 5 best matches
     dream_text = lemmatize(dream_text)
-    df = pd.read_csv("raw_data/dreams_interpretations.csv")
-    embeddings = np.load("raw_data/symbol_embeddings.npy")
-    
+    df = pd.read_csv("Notebooks/raw_data/dreams_interpretations.csv")
+    embeddings = np.load("Notebooks/raw_data/symbol_embeddings.npy")
+
     query_embedding = model.encode([dream_text])
     similarities = np.dot(embeddings, query_embedding.T).flatten()
     top_indices = np.argsort(similarities)[-top_k:][::-1]
-    
+
     results = []
     for idx in top_indices:
         if similarities[idx] > 0.1:  # Only include results with a similarity score above a certain threshold
             results.append({
                 "Dream Symbol": df.iloc[idx]["Dream Symbol"],
                 "Interpretation": df.iloc[idx]["Interpretation"],
-                "score": round(float(similarities[idx]), 4)  
+                "score": round(float(similarities[idx]), 4)
                 })
     return results
 
@@ -52,7 +52,7 @@ def match_dream(dream_text, top_k=5): # 5 best matches
 if __name__ == "__main__":
     dream =  "i dreamed about a birds and horses"
     results = match_dream(dream)
-    
+
     for r in results:
         print(f"\n{r['Dream Symbol']} (score: {r['score']})")
         print(r['Interpretation'])
